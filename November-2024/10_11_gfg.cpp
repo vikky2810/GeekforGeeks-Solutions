4c4a36b8b38fced55b484557d468c69d13fcbe10
Engineervikky <107768078+vikky2810@users.noreply.github.com>
2024-11-10 19:22:14 +0530

    Create 10_11_gfg.cpp

---
+++ November-2024/10_11_gfg.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // a,b : the arrays
+    // Function to return a list containing the union of the two arrays.
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        // Your code here
+        // return vector with correct order of elements
+        vector<int> res;
+        unordered_set<int> s;
+        
+        for(auto i : a)
+        {
+            if(s.find(i) == s.end())
+            {
+                s.insert(i);
+                res.push_back(i);
+            }
+        }
+        for(auto i : b)
+        {
+            if(s.find(i) == s.end())
+            {
+                s.insert(i);
+                res.push_back(i);
+            }
+        }
+        sort(res.begin(),res.end());
+        return res;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a, b;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        // Read second array
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            b.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.findUnion(a, b);
+        for (int i : ans)
+            cout << i << ' ';
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
